28caa178a2ad1ea5fbdd1392108a5353062736d0
Andrew Huff <andrewhuff4@gmail.com>
2016-10-05 20:10:15 -0600

    add code path analysis to always-return
    This will also change the reported location of errors to the inner
    function that failed to return, rather than referring to it's enclosing
    Promise
    
    I bumped up the the version number (2.0.1 -> 3.0.0) because the changed
    location reporting could un-suppress linter errors that have been
    suppressed with `// eslint-disable-line promise/always-return`
    
    fixes xjamundx/eslint-plugin-promise#8
    fixes xjamundx/eslint-plugin-promise#18
    fixes xjamundx/eslint-plugin-promise#24

--- rules/always-return.js
+++ rules/always-return.js
@@ -8,31 +8,88 @@ function isFunctionWithBlockStatement (node) {
   return false
 }
 
-function isReturnOrThrowStatement (node) {
-  return node.type === 'ReturnStatement' || node.type === 'ThrowStatement'
+function isThenExpression (node) {
+  return (
+    node.type === 'CallExpression' &&
+    node.callee.type === 'MemberExpression' &&
+    node.callee.property.name === 'then'
+  )
+}
+
+function isInlineThenFunctionExpression (node) {
+  return (
+    isFunctionWithBlockStatement(node) &&
+    isThenExpression(node.parent)
+  )
+}
+
+function includes (arr, val) {
+  return arr.indexOf(val) !== -1
+}
+
+function last (arr) {
+  return arr[arr.length - 1]
 }
 
 module.exports = {
   create: function (context) {
-    return {
-      MemberExpression: function (node) {
-        var firstArg, body, lastStatement
+    var funcInfoStack = []
+    var CPSIDStack = []
 
-        if (node.property.name !== 'then' || node.parent.type !== 'CallExpression') {
-          return
-        }
+    function isEveryBranchReturning (funcInfo) {
+      // We need to check noCurrentCPSIsOnTheCPSStack because of what
+      // seems like a bug in eslint where 'FunctionExpression:exit' events occur
+      // before all of their constituent codePathSegments have fired their
+      // 'onCodePathSegmentEnd' events
+      var currentIDs = funcInfo.codePath.currentSegments.map(x => x.id)
+      var noCurrentCPSIsOnTheCPSStack = !currentIDs.some((id) => includes(CPSIDStack, id))
 
-        firstArg = node.parent.arguments[0]
-        if (!firstArg || !isFunctionWithBlockStatement(firstArg)) {
-          return
-        }
+      var finalIDs = funcInfo.codePath.finalSegments.map(x => x.id)
+      var everyFinalCPSIsReturning = finalIDs.every((id) => includes(funcInfo.explicitlyReturningCPSIDs, id))
 
-        body = firstArg.body.body
-        lastStatement = body[body.length - 1]
-        if (!lastStatement || !isReturnOrThrowStatement(lastStatement)) {
-          context.report(node, 'Each then() should return a value or throw')
-        }
+      return noCurrentCPSIsOnTheCPSStack && everyFinalCPSIsReturning
+    }
+
+    function onFunctionExpressionExit (node) {
+      if (!isInlineThenFunctionExpression(node)) {
+        return
       }
+
+      var funcInfo = last(funcInfoStack)
+      if (!isEveryBranchReturning(funcInfo)) {
+        context.report(node, 'Each then() should return a value or throw')
+      }
+    }
+
+    function markCurrentCodePathSegmentAsReturning () {
+      var funcInfo = last(funcInfoStack)
+      var currentCPSID = last(CPSIDStack)
+      funcInfo.explicitlyReturningCPSIDs.push(currentCPSID)
+    }
+
+    return {
+      onCodePathStart: function (codePath, node) {
+        funcInfoStack.push({
+          codePath: codePath,
+          explicitlyReturningCPSIDs: []
+        })
+      },
+
+      onCodePathEnd: function (codePath, node) {
+        funcInfoStack.pop()
+      },
+
+      onCodePathSegmentEnd: function (segment, node) {
+        CPSIDStack.pop()
+      },
+      onCodePathSegmentStart: function (segment, node) {
+        CPSIDStack.push(segment.id)
+      },
+
+      ReturnStatement: markCurrentCodePathSegmentAsReturning,
+      ThrowStatement: markCurrentCodePathSegmentAsReturning,
+      'FunctionExpression:exit': onFunctionExpressionExit,
+      'ArrowFunctionExpression:exit': onFunctionExpressionExit
     }
   }
 }
