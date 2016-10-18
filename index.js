2a6d7e7a31860329bac2bdefa784a4b9f960a4c3
Jamund Ferguson <jamund@gmail.com>
2016-10-18 10:23:44 -0700

    fixed bug with no-return-wrap

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = {
   rules: {
     'param-names': require('./rules/param-names'),
+    'no-return-wrap': require('./rules/no-return-wrap'),
     'always-return': require('./rules/always-return'),
     'catch-or-return': require('./rules/catch-or-return'),
     'no-native': require('./rules/no-native')
@@ -8,6 +9,7 @@ module.exports = {
   rulesConfig: {
     'param-names': 1,
     'always-return': 1,
+    'no-return-wrap': 1,
     'no-native': 0,
     'catch-or-return': 1
   }
