c0c662a0906bc5b6b117b9bf454af31249b97b4f
Macklin Underdown <macklinu@users.noreply.github.com>
2018-02-12 09:45:54 -0500

    feat(fixer): add fixer for param-names (#99)
    Resolves #90

--- test/param-names.test.js
+++ test/param-names.test.js
@@ -2,24 +2,43 @@
 
 const rule = require('../rules/param-names')
 const RuleTester = require('eslint').RuleTester
-const ruleTester = new RuleTester()
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaVersion: 6
+  }
+})
 
 const message = 'Promise constructor parameters must be named resolve, reject'
 
 ruleTester.run('param-names', rule, {
   valid: [
-    'new Promise(function(resolve, reject) { })',
-    'new Promise(function(resolve) { })'
+    'new Promise(function(resolve, reject) {})',
+    'new Promise(function(resolve) {})',
+    'new Promise(resolve => {})',
+    'new Promise((resolve, reject) => {})',
+    'new Promise(() => {})'
   ],
 
   invalid: [
     {
-      code: 'new Promise(function(reject, resolve) { })',
-      errors: [{ message }]
+      code: 'new Promise(function(reject, resolve) {})',
+      errors: [{ message }],
+      output: 'new Promise(function(resolve, reject) {})'
     },
     {
-      code: 'new Promise(function(resolve, rej) { })',
-      errors: [{ message }]
+      code: 'new Promise(function(resolve, rej) {})',
+      errors: [{ message }],
+      output: 'new Promise(function(resolve, reject) {})'
+    },
+    {
+      code: 'new Promise(yes => {})',
+      errors: [{ message }],
+      output: 'new Promise(resolve => {})'
+    },
+    {
+      code: 'new Promise((yes, no) => {})',
+      errors: [{ message }],
+      output: 'new Promise((resolve, reject) => {})'
     }
   ]
 })
