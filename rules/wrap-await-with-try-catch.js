865761e7f4d39aff5cd925ab522e567ac5b84a9e
Vedat Can Keklik <vcankeklik@gmail.com>
2020-07-16 21:07:05 +0300

    Refactor and eslint

--- rules/wrap-await-with-try-catch.js
+++ rules/wrap-await-with-try-catch.js
@@ -4,61 +4,86 @@
 
 'use strict'
 
+const getDocsUrl = require('./lib/get-docs-url')
+
 module.exports = {
-    rules: {
-        "wrap-await-with-try-catch": {
-            create: function(context) {
-                function isAwaitHandled() {
-                    var ancestors = context.getAncestors();
-                    var handledInOrder = [];
-
-                    ancestors.forEach((ancestor) => {
-                        if (ancestor.type === "TryStatement") {
-                            handledInOrder.push({name: "try", node: ancestor, relatedTry: ancestor});
-                        }
-                        else if (ancestor.type === "CatchClause") {
-                            handledInOrder.push({name: "catch", node: ancestor, relatedTry: ancestor.parent});
-                        }
-                        else if (ancestor.type === "BlockStatement" && ancestor.parent.finalizer === ancestor) {
-                            handledInOrder.push({name: "finally", node: ancestor, relatedTry: ancestor.parent});
-                        }
-                        else if (ancestor.type === "FunctionExpression" || ancestor.type === "FunctionDeclaration") {
-                            // clear the current parents, we are in a new function
-                            handledInOrder = [];
-                        }
-                    });
-
-                    if (handledInOrder.length === 0) {
-                        return false;
-                    }
-
-                    var lastItem = handledInOrder[handledInOrder.length - 1];
-
-                    while (handledInOrder.length > 0 && !(lastItem.name === "try" && lastItem.node.handler)) {
-                        var tryToBeDeleted = lastItem.relatedTry;
-
-                        while (handledInOrder.length > 0 && lastItem.relatedTry == tryToBeDeleted) {
-                            handledInOrder.pop();
-                            lastItem = handledInOrder[handledInOrder.length - 1];
-                        }
-                    }
-
-                    return handledInOrder.length > 0;
-                }
-
-                return {
-                    AwaitExpression(node) {
-                        if (isAwaitHandled()) {
-                            return;
-                        }
-
-                        context.report({
-                            node: node,
-                            message: '"await"s must be wrapped with a try/catch statement.'
-                        });
-                    }
-                };
-            }
-        }
+  meta: {
+    type: 'suggestion',
+    docs: {
+      url: getDocsUrl('wrap-await-with-try-catch')
     }
-};
+  },
+  create(context) {
+    function isAwaitHandled() {
+      const ancestors = context.getAncestors()
+      let handledInOrder = []
+
+      ancestors.forEach(ancestor => {
+        if (ancestor.type === 'TryStatement') {
+          handledInOrder.push({
+            name: 'try',
+            node: ancestor,
+            relatedTry: ancestor
+          })
+        } else if (ancestor.type === 'CatchClause') {
+          handledInOrder.push({
+            name: 'catch',
+            node: ancestor,
+            relatedTry: ancestor.parent
+          })
+        } else if (
+          ancestor.type === 'BlockStatement' &&
+          ancestor.parent.finalizer === ancestor
+        ) {
+          handledInOrder.push({
+            name: 'finally',
+            node: ancestor,
+            relatedTry: ancestor.parent
+          })
+        } else if (
+          ancestor.type === 'FunctionExpression' ||
+          ancestor.type === 'FunctionDeclaration'
+        ) {
+          // clear the current parents, we are in a new function
+          handledInOrder = []
+        }
+      })
+
+      if (handledInOrder.length === 0) {
+        return false
+      }
+
+      let lastItem = handledInOrder[handledInOrder.length - 1]
+
+      while (
+        handledInOrder.length > 0 &&
+        !(lastItem.name === 'try' && lastItem.node.handler)
+      ) {
+        const tryToBeDeleted = lastItem.relatedTry
+
+        while (
+          handledInOrder.length > 0 &&
+          lastItem.relatedTry == tryToBeDeleted
+        ) {
+          handledInOrder.pop()
+          lastItem = handledInOrder[handledInOrder.length - 1]
+        }
+      }
+
+      return handledInOrder.length > 0
+    }
+
+    return {
+      AwaitExpression(node) {
+        if (isAwaitHandled()) {
+          return
+        }
+
+        context.report({
+          node,
+          message: '"await"s must be wrapped with a try/catch statement.'
+        })
+      }
+    }
+  }
+}
