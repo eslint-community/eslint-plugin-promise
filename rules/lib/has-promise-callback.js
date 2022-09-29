08052e8528ec1ebfd60847801e44b8b2ed5803e1
Yosuke Ota <otameshiyo23@gmail.com>
2022-09-30 07:27:15 +0900

    fix(no-nesting): nested references vars in closure (#361)

--- rules/lib/has-promise-callback.js
+++ rules/lib/has-promise-callback.js
@@ -6,6 +6,26 @@
 
 'use strict'
 
+/**
+ * @typedef {import('estree').SimpleCallExpression} CallExpression
+ * @typedef {import('estree').MemberExpression} MemberExpression
+ * @typedef {import('estree').Identifier} Identifier
+ *
+ * @typedef {object} NameIsThenOrCatch
+ * @property {'then' | 'catch'} name
+ *
+ * @typedef {object} PropertyIsThenOrCatch
+ * @property {Identifier & NameIsThenOrCatch} property
+ *
+ * @typedef {object} CalleeIsPromiseCallback
+ * @property {MemberExpression & PropertyIsThenOrCatch} callee
+ *
+ * @typedef {CallExpression & CalleeIsPromiseCallback} HasPromiseCallback
+ */
+/**
+ * @param {import('estree').Node} node
+ * @returns {node is HasPromiseCallback}
+ */
 function hasPromiseCallback(node) {
   // istanbul ignore if -- only being called within `CallExpression`
   if (node.type !== 'CallExpression') return
