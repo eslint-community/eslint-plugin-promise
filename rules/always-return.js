064637aaa784de119aad6ca77343b42335373d5b
Brett Zamir <brettz9@yahoo.com>
2024-10-17 04:03:37 +0800

    Update rules/always-return.js

--- rules/always-return.js
+++ rules/always-return.js
@@ -173,7 +173,7 @@ module.exports = {
             type: 'array',
             items: {
               type: 'string',
-              pattern: '^\\w+$',
+              pattern: '^[\\w$]+$',
             },
             uniqueItems: true,
           },
